a8e547235af0165d353130b6ae2e94feaddbf4e1
Juan Rivera <juan_rivera99@hotmail.com>
2026-04-09 21:29:02 -0600

    Agregar columna available_for_time_off a incident_code
    Se añadió una nueva columna booleana 'available_for_time_off' a la tabla 'incident_code' mediante una migración. Esta columna permite identificar si un código de incidente está disponible para solicitudes de tiempo libre. También se actualizó el snapshot del modelo para reflejar este cambio.

---
+++ PrenominaApi/Data/Migrations/Prenomina/20260410031357_AddColumnOnlyTimeOfRequestInIncidentCode.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PrenominaApi.Data.Migrations.Prenomina
+{
+    /// <inheritdoc />
+    public partial class AddColumnOnlyTimeOfRequestInIncidentCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "available_for_time_off",
+                table: "incident_code",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "available_for_time_off",
+                table: "incident_code");
+        }
+    }
+}
